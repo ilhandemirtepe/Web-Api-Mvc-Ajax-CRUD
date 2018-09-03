7bdb282a56bdacfbc688d588143739f61c464830
ilhan <ilhandemirtepe@gmail.com>
2018-09-03 10:42:03 +0300

    web api,ajax crud
    web api,ajax crud

---
+++ C#/StudentDetailsServiceLayer/Controllers/StudentController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using StudentDetailsServiceLayer.Models;
+
+namespace StudentDetailsServiceLayer.Controllers
+{
+    public class StudentController : ApiController
+    {
+        static readonly IStudentRepository studentRepository = new StudentRepository();
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return studentRepository.GetAll();
+        }
+
+        public HttpResponseMessage GetStudent(int id)
+        {
+            Student student = studentRepository.Get(id);
+            if (student == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound,"ID verilen öğrenci bulunmuyor");
+            }
+
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, student);
+            }
+        }
+
+        public IEnumerable<Student> GetStudentsByGender(string gender)
+        {
+            return studentRepository.GetAll().Where(
+                s => string.Equals(s.gender, gender, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public IEnumerable<Student> GetStudentsByAge(int age)
+        {
+            return studentRepository.GetAll().Where(
+                s => string.Equals(s.age.ToString(), age.ToString(), StringComparison.OrdinalIgnoreCase));
+        }
+
+        public HttpResponseMessage PostStudent(Student student)
+        {
+            student = studentRepository.Add(student);
+            var response = Request.CreateResponse<Student>(HttpStatusCode.Created, student);
+            string uri = Url.Link("DefaultApi", new { id = student.id });
+            response.Headers.Location = new Uri(uri);
+            return response;
+        }
+
+        public HttpResponseMessage PutStudent(int id, Student student)
+        {
+            student.id = id;
+            if (!studentRepository.Update(student))
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "ID verilen öğrenci güncellenmiyor");
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+        }
+
+        public HttpResponseMessage DeleteProduct(int id)
+        {
+            studentRepository.Remove(id);
+            return new HttpResponseMessage(HttpStatusCode.NoContent);
+        }
+    }
+}
---
+++ C#/StudentDetailsServiceLayer/Models/StudentRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentDetailsServiceLayer.Models
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private List<Student> students = new List<Student>();
+        public StudentRepository()
+        {
+            Add(new Student { name = "ali", id = 1, gender = "erkek", age = 15 });
+            Add(new Student { name = "ayşe", id = 2, gender = "kız", age = 14 });
+            Add(new Student { name = "ilhan", id = 3, gender = "erkek", age = 15 });
+            Add(new Student { name = "hatice", id = 4, gender = "kız", age = 15 });
+        }
+
+        public IEnumerable<Student> GetAll()
+        {
+            return students;
+        }
+
+        public Student Get(int id)
+        {
+            return students.Find(s => s.id == id);
+        }
+
+        public Student Add(Student student)
+        {
+            if (student == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+            students.Add(student);
+            return student;
+        }
+
+        public void Remove(int id)
+        {
+            students.RemoveAll(s => s.id == id);
+        }
+
+        public bool Update(Student student)
+        {
+            if (student == null)
+            {
+                throw new ArgumentNullException("student");
+            }
+            int index = students.FindIndex(s => s.id == student.id);
+            if (index == -1)
+            {
+                return false;
+            }
+            students.RemoveAt(index);
+            students.Add(student);
+            return true;
+        }
+    }
+}
